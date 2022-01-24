3eab16f0d32a8f12dce7afc6712af0386b427bf8
sehaj97 <sehajmagan@gmail.com>
2022-01-24 12:38:00 -0500

    added thoughts functionality

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const usersRoutes = require('./user-routes');
+const thoughtsRoutes = require('./thought-routes');
 
 // Add `/users` to created routes
 router.use('/users', usersRoutes);
+router.use('/thoughts', thoughtsRoutes);
 
 // Export Module Router
 module.exports = router;
--- api/user-routes.js
+++ api/user-routes.js
@@ -13,13 +13,23 @@ const {
 } = require('../controllers/users-controller');
 
 // -- Directs to: /api/users <GET, POST>
-router.route('/').get(getAllUsers).post(createUsers);
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUsers);
 
 // -- Directs to: /api/users/:id <GET, PUT, DELETE>
-router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+router
+    .route('/:id')
+    .get(getUsersById)
+    .put(updateUsers)
+    .delete(deleteUsers);
 
 // -- Directs to: /api/users/:userId/friends/:friendId <POST, DELETE>
-router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
 
 // Module export router
 module.exports = router;
